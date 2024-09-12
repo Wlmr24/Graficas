372c7010398184b5b3247dd6f19172f3fc058f0f
wlmr24 <wilmergr24@gmail.com>
2024-09-12 18:27:54 -0500

    Graficas

---
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-barras',
+  templateUrl: './barras.component.html',
+  styleUrl: './barras.component.css'
+})
+export class BarrasComponent implements OnInit {
+
+
+
+constructor () {}
+
+  ngOnInit(): void {
+   
+  }
+
+
+}
